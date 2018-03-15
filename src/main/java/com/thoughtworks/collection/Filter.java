2283b1438136075f223f348f0af2276aa9dd66e2
Hong Ma <hma@thoughtworks.com>
2018-03-15 18:10:58 +0800

    delete some filter funtion

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,7 +1,5 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.*;
 
 public class Filter {
@@ -48,67 +46,11 @@ public class Filter {
         return result;
     }
 
-    public Map<Integer, Integer> getMapOfArrayList() {
-        Map map = new HashMap<Integer,Integer>();
-
-        for(int i=0; i<array.size(); i++){
-
-            Integer element = array.get(i);
-            if(map.containsKey(element)){
-
-                Integer previousValue = (Integer) map.get(element);
-                map.put(element, ++previousValue);
-            }else{
-                map.put(element, 1);
-            }
-        }
-
-        return map;
-    }
-
-    public List<List<Integer>> getDividedArray() {
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        for(int i=0; i<array.size(); i++){
-            int num = 0;
-            for(int j=0; j<result.size(); j++){
-
-                if(result.get(j).contains(array.get(i))){
-                    result.get(j).add(array.get(i));
-                    break;
-                }else {
-                    num++;
-                }
-            }
-            if(num == result.size()){
-                List<Integer> sonList = new ArrayList<>();
-                sonList.add(array.get(i));
-                result.add(sonList);
-            }
-        }
-
-        return result;
-    }
-
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
         List<Integer> result = new ArrayList<>();
 
         for(int i=0; i<firstList.size(); i++){
             if(secondList.contains(firstList.get(i))){
-                result.add(firstList.get(i));
-            }
-        }
-
-        return result;
-    }
-
-    public List<Integer> getUncommonElements(List<Integer> firstList, List<Integer> secondList) {
-
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0; i<firstList.size(); i++){
-            if(!secondList.contains(firstList.get(i))){
                 result.add(firstList.get(i));
             }
         }
--- src/test/java/com/thoughtworks/collection/FilterTest.java
+++ src/test/java/com/thoughtworks/collection/FilterTest.java
@@ -49,36 +49,6 @@ public class FilterTest {
     }
 
     @Test
-    public void should_divide_array_and_return_number(){
-        Integer[] array = new Integer[]{1,1,1,1,2,3,1,3,4,2,3,1,3,4,2};
-        List<Integer> arrayList = Arrays.asList(array);
-
-        Filter filter = new Filter(arrayList);
-
-        Map result = new HashMap<>();
-        result.put(1,6);
-        result.put(2,3);
-        result.put(3,4);
-        result.put(4,2);
-
-        assertThat(filter.getMapOfArrayList()).isEqualTo(result);
-    }
-
-    @Test
-    public void should_return_divided_array(){
-        Integer[] array = new Integer[]{1,1,1,1,2,3,1,3,4,2,3,1,3,4,2};
-        List<Integer> arrayList = Arrays.asList(array);
-
-        Filter filter = new Filter(arrayList);
-
-        assertThat(filter.getDividedArray().size()).isEqualTo(4);
-        assertThat(filter.getDividedArray().get(0).size()).isEqualTo(6);
-        assertThat(filter.getDividedArray().get(1).size()).isEqualTo(3);
-        assertThat(filter.getDividedArray().get(2).size()).isEqualTo(4);
-        assertThat(filter.getDividedArray().get(3).size()).isEqualTo(2);
-    }
-
-    @Test
     public void should_return_common_elements(){
         Integer[] array = new Integer[] {6,2,3,4,9};
         Integer[] array1 = new Integer[] {2,3,5,9,6};
@@ -94,19 +64,4 @@ public class FilterTest {
         assertThat(filter.getCommonElements(arrayList, arrayList1)).isEqualTo(resultList);
     }
 
-    @Test
-    public void should_return_uncommon_elements(){
-        Integer[] array = new Integer[] {6,2,3,4,9};
-        Integer[] array1 = new Integer[] {2,3,5,9,6};
-
-        List<Integer> arrayList =  Arrays.asList(array);
-        List<Integer> arrayList1 =  Arrays.asList(array1);
-
-        Filter filter = new Filter(arrayList);
-
-        Integer[] result = new Integer[] {4};
-        List<Integer> resultList = Arrays.asList(result);
-
-        assertThat(filter.getUncommonElements(arrayList, arrayList1)).isEqualTo(resultList);
-    }
 }
