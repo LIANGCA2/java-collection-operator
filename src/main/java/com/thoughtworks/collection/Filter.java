04d868e373afff93257db4a2c7ac8f632f4e5fde
zjx-immersion <jianxin_home@163.com>
2018-07-11 11:17:37 +0800

    [Jason]refactored the class filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,61 +1,31 @@
 package com.thoughtworks.collection;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
         this.array = array;
     }
 
     public List<Integer> filterEven() {
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0; i<array.size(); i++){
-            if(array.get(i)%2 == 0){
-                result.add(array.get(i));
-            }
-        }
-
-        return result;
+        return array.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0; i<array.size(); i++){
-            if(array.get(i)%3 == 0){
-                result.add(array.get(i));
-            }
-        }
-
-        return result;
+        return array.stream().filter(num -> num % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        List<Integer> result = new ArrayList<>();
 
-        for(int i=0; i<array.size(); i++){
-            if(!result.contains(array.get(i))){
-                result.add(array.get(i));
-            }
-        }
-
-        return result;
+        return array.stream().distinct().collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0; i<firstList.size(); i++){
-            if(secondList.contains(firstList.get(i))){
-                result.add(firstList.get(i));
-            }
-        }
-
-        return result;
+        return firstList.stream().filter(num -> secondList.stream().anyMatch(num::equals)).collect(Collectors.toList());
     }
 
 }
